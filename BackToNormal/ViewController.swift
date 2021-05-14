10afb661866626cf81a41b6d45bbb539728b6f12
russellm <russell@mirabellifamily.org>
2021-05-14 09:07:48 -0500

    Initial Commit

---
+++ BackToNormal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BackToNormal
+//
+//  Created by Russell Mirabelli on 5/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
